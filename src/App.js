6f2e11e91ee1daa382e9803fea53a238f3d3cfa5
Alejandro Gil Casco <alexgilcasco@gmail.com>
2022-01-07 19:31:45 +0900

    Keys and Buttons working

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { Pad } from "./Functions";
+import keySounds from "./KeySounds";
 
 function App() {
+
   return (
     <div className="App">
+      
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h2>Drum Machine</h2>
       </header>
+      <div className="drum-machine">
+        
+        <div id="drum-machine" className="container">
+          {keySounds.map((sound) => (
+            <Pad key={sound.id} sound={sound}  className="drum-pad" id={sound.id} />
+          ))}
+
+          <br />
+
+          <p>Created from Alejandro as a FCC project</p>
+        </div>
+      </div>
     </div>
   );
 }
