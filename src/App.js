5bb2eac07df015ee26e1d46340e16760e1ddf780
Alejandro Gil Casco <alexgilcasco@gmail.com>
2022-01-08 21:10:10 +0900

    Removed PicoCSS. Fast design with css

--- src/App.js
+++ src/App.js
@@ -4,27 +4,35 @@ import { Pad } from "./Functions";
 import keySounds from "./KeySounds";
 
 function App() {
- const [pressedKeys, record] = React.useState("");
+  const [pressedKeys, record] = React.useState("");
   return (
     <div className="App">
-      
-     
       <div className="drum-machine container">
-      <header className="App-header">
-        <h2>Drum Machine</h2>
-      </header>
+        <header className="App-header">
+          <h2>Drum Machine</h2>
+        </header>
         <div id="display" className="wrapper">
           {keySounds.map((sound) => (
-            <Pad key={sound.id} sound={sound}  className="drum-pad" id={sound.id} record={record} />
+            <Pad
+              key={sound.id}
+              sound={sound}
+              className="drum-pad"
+              id={sound.id}
+              record={record}
+            />
           ))}
-          </div>
-          <br />
-          {/* Show they key trigger you pressed */}
+        </div>
+        <br />
 
-          <h3 class>You pressed: {pressedKeys}</h3>
-          <br />
-          <p>Created by Alejandro for FreeCodeCamp</p>
-        
+        <h3 class>You pressed: {pressedKeys}</h3>
+        <br />
+        <p>
+          Created by{" "}
+          <a target="_blank" rel="noreferrer" href="https://github.com/deku-95">
+            Alejandro
+          </a>{" "}
+          for FreeCodeCamp
+        </p>
       </div>
     </div>
   );
--- src/Functions.jsx
+++ src/Functions.jsx
@@ -1,39 +1,38 @@
 import React from "react";
 
-
 function Pad({ sound, record }) {
-    const [active, setActive] = React.useState(false);
-  
-    React.useEffect(() => {
-      document.addEventListener("keydown", handleKeyPress);
-      return () => {
-        document.removeEventListener("keydown", handleKeyPress);
-      };
-    });
-  
-    const handleKeyPress = (e) => {
-      if (e.keyCode === sound.keyCode) {
-        playSound();
-      }
-    };
-  
-    const playSound = () => {
-      const audio = document.getElementById(sound.keyTrigger);
-      setActive(true);
-      setTimeout(() => setActive(false), 200);
-      audio.currentTime = 0;
-      audio.play();
-      record(sound.id + "");
-    };
-  
-    return (
-      <div className="wrapper">
-        <button onClick={playSound} className={`drum-pad ${active && 'secondary'}`}>
-          <audio className="clip" id={sound.keyTrigger} src={sound.url} />
-          {sound.keyTrigger}
-        </button>
-      </div>
-    );
-  }
+  const [active, setActive] = React.useState(false);
 
-export {Pad};
+  React.useEffect(() => {
+    document.addEventListener("keydown", handleKeyPress);
+    return () => {
+      document.removeEventListener("keydown", handleKeyPress);
+    };
+  });
+
+  const handleKeyPress = (e) => {
+    if (e.keyCode === sound.keyCode) {
+      playSound();
+    }
+  };
+
+  const playSound = () => {
+    const audio = document.getElementById(sound.keyTrigger);
+    setActive(true);
+    setTimeout(() => setActive(false), 150);
+    audio.currentTime = 0;
+    audio.play();
+    record(sound.id + "");
+  };
+
+  return (
+    <div className="wrapper">
+      <button onClick={playSound} className={`drum-pad ${active && "active"}`}>
+        <audio className="clip" id={sound.keyTrigger} src={sound.url} />
+        {sound.keyTrigger}
+      </button>
+    </div>
+  );
+}
+
+export { Pad };
